7220c804cfd7c3e2d18d95151fde84c38b1f11ae
james Johnson <2013matthew2013@gmail.com>
2020-02-02 01:49:42 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -57,7 +57,7 @@ holiday_hash.each do |season, h|
   h.each do |holiday, array|
     str = holiday.to_s.split("_")
     str.map {|w| w.capitalize}.join()
-    new_str = str.map {|w| w.capitalize}.join("  ")
+    new_str = str.map {|w| w.capitalize}.join(" ")
     supply_str = array.join(", ")
     puts "  #{new_str}: #{supply_str}"
   end
